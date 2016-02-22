bccb2de7c9adcfe55072aab82255993eb0b3e834
jabiel <jabiel@o2.pl>
2016-02-22 13:28:59 +0100

    Added admob

--- Fuel/www/js/app/app.js
+++ Fuel/www/js/app/app.js
@@ -1,24 +1,38 @@
 // Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+
+        if (!AdMob) { alert('admob plugin not ready'); return; }
+        //console.log('AdMob', AdMob);
+        //AdMob.setOptions({
+        //    // adSize: 'SMART_BANNER',
+        //    // width: integer, // valid when set adSize 'CUSTOM'
+        //    // height: integer, // valid when set adSize 'CUSTOM'
+        //    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+        //    // offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
+        //    bgColor: 'black', // color name, or '#RRGGBB'
+        //    // x: integer,    // valid when set position to 0 / POS_XY
+        //    // y: integer,    // valid when set position to 0 / POS_XY
+        //    isTesting: true, // set to true, to receiving test ad for testing purpose
+        //    // autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
+        //});
+        AdMob.createBanner({
+            adId: 'ca-app-pub-0757678805524137/4974429284',
+            position: AdMob.AD_POSITION.BOTTOM_CENTER,
+            autoShow: true
+        });
+        
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleLightContent();
+        }
+    });
 })
 
 .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
@@ -26,17 +40,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
     $ionicConfigProvider.platform.android.tabs.position('bottom');
     
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
     $stateProvider
 
     // setup an abstract state for the tabs directive
     .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
     })
 
     // Each tab has its own nav history stack:
